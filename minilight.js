176d1c273dd1bf0cb6a9b558c67437eec8a18ff0
Darius Bacon <darius@wry.me>
2012-01-14 15:26:02 -0800

    add missing param

--- minilight.js
+++ minilight.js
@@ -1,6 +1,6 @@
 // TODO: break up calculation into timeslices or use webworkers
-function minilight(image, iterations, camera, scene) {
+function minilight(image, iterations, camera, scene, random) {
     for (var frameNum = 1; frameNum <= iterations; ++frameNum)
-        camera.getFrame(scene, image);
+        camera.getFrame(scene, image, random);
     return image;
 }
